67cedb9e86a6af7d0b77090a67e179e6a479e4fc
Chris Hall <crh3333@gmail.com>
2011-09-07 00:08:10 -0400

    Moved photos back to the remote backup and created a launch agent for the car sync script.

---
+++ scripts/backup/net.mod10.sync_tdi.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+drive="/Volumes/TDI"
+src="$HOME/Music/iTunes/iTunes Media/Music"
+dst="$drive"
+
+if [ -d "$drive" ]; then
+	/usr/local/bin/growlnotify -t "TDI" -m "Synchronizing music."
+	if [ ! -d "$dst" ]; then
+		mkdir -p "$dst"
+	fi
+	rsync -av --del "$src" "$dst"
+	/usr/local/bin/growlnotify -t "TDI" -m "Synchronization complete."
+fi
+
