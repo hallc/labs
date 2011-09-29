d4d12ff3f19ecb2b78b7262424575e535a3cad84
Chris Hall <crh3333@gmail.com>
2011-09-29 17:31:09 -0400

    Updated the music backup script.

--- scripts/music_backup.sh
+++ scripts/music_backup.sh
@@ -5,12 +5,12 @@ src="$HOME/Music/iTunes/iTunes Media/Music/"
 dst="$drive/Backup/Music"
 
 if [ -d "$drive" ]; then
-	/usr/local/bin/growlnotify -t "BACKUP" -m "Starting local backup."
+	/usr/local/bin/growlnotify -t "BACKUP" -m "Starting music backup."
 
 	if [ ! -d "$dst" ]; then
 		mkdir -p "$dst"
 	fi
-	rsync -aq --del "$src" "$dst"
+	rsync -a --del "$src" "$dst"
 
-    /usr/local/bin/growlnotify -t "BACKUP" -m "Local backup complete."
+    /usr/local/bin/growlnotify -t "BACKUP" -m "Music backup complete."
 fi
