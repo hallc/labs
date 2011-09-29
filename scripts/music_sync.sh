6933b62a7a9b5d96ca0d5b009e658cd14881df04
Chris Hall <crh3333@gmail.com>
2011-09-29 18:21:46 -0400

    Updated music sync script.

--- scripts/music_sync.sh
+++ scripts/music_sync.sh
@@ -5,11 +5,11 @@ src="$HOME/Music/iTunes/iTunes Media/Music"
 dst="$drive"
 
 if [ -d "$drive" ]; then
-	/usr/local/bin/growlnotify -t "TDI" -m "Synchronizing music."
+	/usr/local/bin/growlnotify -t "MUSIC" -m "Synchronizing music."
 	if [ ! -d "$dst" ]; then
 		mkdir -p "$dst"
 	fi
 	rsync -av --del "$src" "$dst"
-	/usr/local/bin/growlnotify -t "TDI" -m "Synchronization complete."
+	/usr/local/bin/growlnotify -t "MUSIC" -m "Music synchronization complete."
 fi
 
