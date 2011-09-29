b1aae08bd727003609bfa9be73d86ea8c4648021
Chris Hall <crh3333@gmail.com>
2011-09-29 14:35:59 -0400

    Updated the lion script to hide the "RDC Connections" directory.

--- scripts/lion.sh
+++ scripts/lion.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-chflags nohidden ~/Library/
-#defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool NO
-#defaults write -g ApplePressAndHoldEnabled -bool false
-#defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool NO
-#defaults write -g AppleShowScrollBars -string Always
+chflags nohidden ~/Library
+chflags hidden ~/Documents/RDC\ Connections
+
