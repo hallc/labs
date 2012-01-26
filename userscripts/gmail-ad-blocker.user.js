6e9a02a94dfedf21e78dd93c65b712a48bc7649e
Chris Hall <chris.hall@mod10.net>
2012-01-25 22:13:18 -0500

    Adding userscripts.

---
+++ userscripts/gmail-ad-blocker.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name           Gmail Ad Blocker
+// @namespace      net.mod10.userscripts.gab
+// @include        http*://mail.google.com/*
+// ==/UserScript==
+
+GM_addStyle('.oM { display:none; }'); // message right sidebar
+GM_addStyle('.PS { display:none; }'); // message hottom ad
+
---
+++ userscripts/google-clean-signin.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name           Google Clean Sign-In
+// @namespace      net.mod10.userscripts.gcs
+// @include        http*://accounts.google.com/*
+// ==/UserScript==
+
+GM_addStyle('.product-info { display: none; }'); // left side content
+GM_addStyle('.sign-in { float: initial; margin: 0 auto; }'); // center sign-in box
+
