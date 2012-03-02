d4eed36c1dc23773686843b3dcb9261a3f9dec2e
Chris Hall <chris.hall@mod10.net>
2012-03-01 19:17:20 -0500

    Replaced /Users/crh with ~/.

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /usr/local/bin/growlnotify -t "BACKUP" -m "Starting backup."
-(time rsync -azvh --del --delete-excluded --include="/Desktop" --include "/Documents" --include "/Music" --include "/Pictures" --include "/Projects" --include "/Sites" --exclude "/*" ~/ $RSYNC_ACCOUNT:$USER) &> $HOME/.backup.log
+(time rsync -azvh --del --delete-excluded --include="/Desktop" --include "/Documents" --include "/Music" --include "/Pictures" --include "/Projects" --include "/Sites" --exclude "/*" ~/ $RSYNC_ACCOUNT:~/) &> ~/.backup.log
 /usr/local/bin/growlnotify -t "BACKUP" -m "Backup complete."
 
