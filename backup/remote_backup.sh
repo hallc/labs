53f7711a6d4b8b86ae2dbd0f9a71639f22586357
Chris Hall <chris.hall@mod10.net>
2012-06-13 20:32:14 -0400

    Adding dominions3 saves & config to the backup scripts.

--- backup/remote_backup.sh
+++ backup/remote_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /usr/local/bin/growlnotify -t "BACKUP" -m "Starting remote backup."
-(time rsync -azvh --del --delete-excluded --include="/Desktop" --include "/Documents" --include "/Pictures" --exclude "/Pictures/Aperture Library.aplibrary/Thumbnails/*" --exclude "/Pictures/Aperture Library.aplibrary/Previews/*" --include "/Projects" --include "/Sites" --exclude "/*" ~/ $REMOTE_RSYNC_ACCOUNT:$USER) &> ~/.remote_backup.log
+(time rsync -azvh --del --delete-excluded --include="/Desktop" --include "/Documents" --include "/Pictures" --exclude "/Pictures/Aperture Library.aplibrary/Thumbnails/*" --exclude "/Pictures/Aperture Library.aplibrary/Previews/*" --include "/Projects" --include "/Sites" --include "/dominions3" --exclude "/*" ~/ $REMOTE_RSYNC_ACCOUNT:$USER) &> ~/.remote_backup.log
 /usr/local/bin/growlnotify -t "BACKUP" -m "Remote backup complete."
 
