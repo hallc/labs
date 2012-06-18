4c04af1026237532add7958c5495c293accdb58e
Chris Hall <crh@trifecta.com>
2012-06-18 14:39:27 -0400

    Updated print size script to output more meaningful info.

--- scripts/px.py
+++ scripts/px.py
@@ -8,10 +8,14 @@ def main():
 	height = math.sqrt(pixels/1.5)
 	width = height * 1.5
 
-	resolutions = (150, 288, 300, 600)
+	sizes = [(4, 6), (8, 10), (11, 14), (13, 19), (24, 36)]
+	for size in sizes:
+		dpi_v = round(height / size[1])
+		dpi_h = round(width / size[0])
 
-	for resolution in resolutions:
-		print('{0}dpi: {1}"x{2}"'.format(resolution, round(height/resolution, 1), round(width/resolution, 1)))
+		dpi = min(dpi_v, dpi_h)
+
+		print('{}x{}":\t{:>3.0f} dpi'.format(size[0], size[1], dpi))
 
 if __name__ == '__main__':
 	main()
