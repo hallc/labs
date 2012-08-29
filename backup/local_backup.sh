04b1649284b046e6bbb02a086d121e2a60ce737d
Chris Hall <chris.hall@mod10.net>
2012-08-28 22:14:32 -0400

    The backup scripts now replace their start message with the completion message.

--- backup/local_backup.sh
+++ backup/local_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-terminal-notifier -title "BACKUP" -message "Starting local backup."
+terminal-notifier -group "$0" -title "BACKUP" -message "Starting local backup."
 (time rsync -azvh --del --delete-excluded --include="/Desktop" --include "/Documents" --include "/Music" --include "/Pictures" --include "/Projects" --include "/Sites" --include "/dominions3" --exclude "/*" ~/ $LOCAL_RSYNC_ACCOUNT:$USER) &> ~/.local_backup.log
-terminal-notifier -title "BACKUP" -message "Local backup complete."
+terminal-notifier -group "$0" -title "BACKUP" -message "Local backup complete."
 
--- backup/music_sync.sh
+++ backup/music_sync.sh
@@ -5,11 +5,11 @@ src="$HOME/Music/iTunes/iTunes Media/Music"
 dst="$drive"
 
 if [ -d "$drive" ]; then
-	terminal-notifier -title "MUSIC" -message "Synchronizing music."
+	terminal-notifier -group "$0" -title "MUSIC" -message "Synchronizing music."
 	if [ ! -d "$dst" ]; then
 		mkdir -p "$dst"
 	fi
 	rsync -avh --del "$src" "$dst"
-	terminal-notifier -title "MUSIC" -message "Music synchronization complete."
+	terminal-notifier -group "$0" -title "MUSIC" -message "Music synchronization complete."
 fi
 
