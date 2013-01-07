aed2bacac1e99fb6ce02bb7a10d90920822ceb6e
Chris Hall <chris.hall@mod10.net>
2013-01-07 09:01:13 -0500

    Updated lion config script.

--- scripts/lion.sh
+++ scripts/lion.sh
@@ -4,10 +4,10 @@ chflags nohidden ~/Library
 chflags hidden ~/Documents/RDC\ Connections
 chflags hidden ~/Documents/Microsoft\ User\ Data
 
-defaults write -g ApplePressAndHoldEnabled -bool false
+#defaults write -g ApplePressAndHoldEnabled -bool false
 
 #sudo systemsetup -setkernelbootarchitecture i386
-sudo systemsetup -setkernelbootarchitecture x86_64
+#sudo systemsetup -setkernelbootarchitecture x86_64
 
 sudo pmset -a hibernatemode 0
 
