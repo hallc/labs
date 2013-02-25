f8c98cfa075489691f383e2cb6fd78861ea25253
Chris Hall <crh@trifecta.com>
2013-02-25 15:39:25 -0500

    Added 'Open With' menu clean-up command.

--- scripts/lion.sh
+++ scripts/lion.sh
@@ -11,3 +11,5 @@ chflags hidden ~/Documents/Microsoft\ User\ Data
 
 sudo pmset -a hibernatemode 0
 
+/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain system -domain user
+
