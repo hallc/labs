50f98f03736ce9169a640784acf67bc58f255667
Chris Hall <chris.hall@mod10.net>
2013-10-15 12:01:18 -0400

    Updated backup script.

--- backup/local_backup.sh
+++ backup/local_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 terminal-notifier -group "$0" -title "BACKUP" -message "Starting local backup."
-(time rsync -azvh --del --delete-excluded --include="/Desktop" --include "/Documents" --include "/Music" --include "/Pictures" --include "/Projects" --include "/Sites" --include "/dominions3" --exclude "/*" ~/ $LOCAL_RSYNC_ACCOUNT:$USER) &> ~/.local_backup.log
+(time rsync -azvh --del --delete-excluded --include="/Desktop" --include "/Documents" --include "/Music" --include "/Pictures" --include "/Projects" --include "/Sites" --exclude "/*" ~/ $LOCAL_RSYNC_ACCOUNT:$USER) &> ~/.local_backup.log
 terminal-notifier -group "$0" -title "BACKUP" -message "Local backup complete."
 
