b56d9d516010b6322a65e31509b2788ad3076f30
Chris Hall <chris.hall@mod10.net>
2013-10-22 22:17:56 -0400

    Updated config script for Mavericks.

--- scripts/mavericks.sh
+++ scripts/mavericks.sh
@@ -4,11 +4,6 @@ chflags nohidden ~/Library
 chflags hidden ~/Documents/RDC\ Connections
 chflags hidden ~/Documents/Microsoft\ User\ Data
 
-#defaults write -g ApplePressAndHoldEnabled -bool false
-
-#sudo systemsetup -setkernelbootarchitecture i386
-#sudo systemsetup -setkernelbootarchitecture x86_64
-
 sudo pmset -a hibernatemode 0
 
 /System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain system -domain user
