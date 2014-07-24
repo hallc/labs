ba14db4219ce428868b4ab851cfda63a633198da
Chris Hall <chris.hall@mod10.net>
2014-07-24 13:15:34 -0400

    The avd fix script now resets both the x and y axes.

--- scripts/avdfix.sh
+++ scripts/avdfix.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 for file in `find ~/.android/avd/ -name emulator-user.ini`; do
-	sed -i 's/window\.y = .*/window.y = 0/' $file
+	sed -i -e 's/window\.y = .*/window.y = 0/' "$file"
+	sed -i -e 's/window\.x = .*/window.x = 0/' "$file"
 done
