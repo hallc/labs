a0773d0e0d165c8f061e64268271328f42e18edb
Chris Hall <chris.hall@mod10.net>
2017-03-08 23:17:36 -0500

    Removing some obsolete scripts.

--- scripts/avdfix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in `find ~/.android/avd/ -name emulator-user.ini`; do
-	sed -i -e 's/window\.y = .*/window.y = 0/' "$file"
-	sed -i -e 's/window\.x = .*/window.x = 0/' "$file"
-done
