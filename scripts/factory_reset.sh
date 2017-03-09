7a4b8ef92201cd2ca0924d12ac8eebab8eb3f476
Chris Hall <chris.hall@mod10.net>
2017-03-08 23:54:33 -0500

    Updated factory reset script to handle iPad format changes.

--- scripts/factory_reset.sh
+++ scripts/factory_reset.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-for uid in `xcrun simctl list | grep Shutdown | sed 's/[^\(]*(\([^)]*\)).*/\1/'`; do
+for uid in `xcrun simctl list | grep Shutdown | sed -E 's/.*([A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}).*/\1/'`; do
 	xcrun simctl erase $uid
 done
-
