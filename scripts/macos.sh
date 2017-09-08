330df619a6172cb3e363565a15eb01dfab249b73
Chris Hall <chris.hall@mod10.net>
2017-09-08 10:22:58 -0400

    Updating macOS config script

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -20,9 +20,16 @@ defaults write com.apple.coreservices.uiagent CSUILastOSVersionWhereSafariRecomm
 defaults write com.apple.Safari DefaultBrowserDateOfLastPrompt -date '2050-01-01T00:00:00Z'
 defaults write com.apple.Safari DefaultBrowserPromptingState -int 2
 
+# Disable Hyperlink Auditing Beacon
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2HyperlinkAuditingEnabled -bool false
+
 # Disable HotPlug for cameras
 defaults write com.apple.ImageCapture disableHotPlug -bool YES
 
 # Disable metadata file creation on network and USB drives
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable captive portal detection
+defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control Active -boolean false
+
