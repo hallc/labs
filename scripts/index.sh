2c0e8edaf10f3c45934fa8ef5f050498f4e9bdf9
Chris Hall <chris.hall@mod10.net>
2022-12-22 08:43:57 -0500

    Improve macOS metadata management script

--- scripts/index.sh
+++ scripts/index.sh
@@ -9,4 +9,10 @@ if [ $# != 2 ] || ! [ -d "$2" ] || ( [ $1 != "on" ] && [ $1 != "off" ] ); then
 	exit 1
 fi
 
+if [ $1 = "on" ]; then
+	touch "$1/.metadata_never_index"
+else
+	rm "$1/.metadata_never_index"
+fi
+
 mdutil -i $1 "$2"
