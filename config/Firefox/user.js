91f22562f8f211a7852452d9bdb1e5db7385b4be
Chris Hall <crh3333@gmail.com>
2024-10-01 09:43:46 -0400

    Create Firefox userChrome.css

--- config/Firefox/user.js
+++ config/Firefox/user.js
@@ -1,3 +1,4 @@
 user_pref("browser.tabs.tabmanager.enabled", false);
 user_pref("browser.urlbar.update2.engineAliasRefresh", true);
 user_pref("extensions.pocket.enabled", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
