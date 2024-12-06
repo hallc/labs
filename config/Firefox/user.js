a5a5345cffb08d83972ee45de395ad6d07025e21
Chris Hall <crh3333@gmail.com>
2024-12-05 20:14:56 -0500

    Update Firefox user.js

--- config/Firefox/user.js
+++ config/Firefox/user.js
@@ -1,4 +1,6 @@
+user_pref("browser.tabs.hoverPreview.enabled", false);
 user_pref("browser.tabs.tabmanager.enabled", false);
 user_pref("browser.urlbar.update2.engineAliasRefresh", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
