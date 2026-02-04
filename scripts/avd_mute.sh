38e414baa8e4b1fe3a5f706f5066d6c366bdb8ed
Chris Hall <crh3333@gmail.com>
2026-02-04 12:52:39 -0500

    Fix missing output redirect in avd_mute.sh

--- scripts/avd_mute.sh
+++ scripts/avd_mute.sh
@@ -2,6 +2,6 @@
 
 for file in $(find ~/.android/avd -name 'config.ini' -mindepth 2 -maxdepth 2)
 do
-	sed 's/hw.audioInput=yes/hw.audioInput=no/' < "$file" "$file.tmp"
+	sed 's/hw.audioInput=yes/hw.audioInput=no/' < "$file" > "$file.tmp"
 	mv "$file.tmp" "$file"
 done
