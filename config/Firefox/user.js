4fcbff3e9d2f756300030e6cf2ef7355c24dd594
Chris Hall <crh3333@gmail.com>
2026-08-27 10:44:12 -0400

    Disable Firefox tab split view

--- config/Firefox/user.js
+++ config/Firefox/user.js
@@ -1,5 +1,6 @@
 user_pref("browser.tabs.groups.enabled", false);
 user_pref("browser.tabs.hoverPreview.enabled", false);
+user_pref("browser.tabs.splitView.enabled", false);
 user_pref("browser.urlbar.trimURLs", false);
 user_pref("mousewheel.with_alt.action", -1);
 user_pref("mousewheel.with_control.action", -1);
